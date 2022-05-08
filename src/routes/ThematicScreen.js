6e1c637fc9052840cb8925abc00fdb83bcaa14e9
GKouv89 <gina101@windowslive.com>
2022-05-08 10:20:11 -0700

    Breadcrumb respecting the router added.

--- src/routes/ThematicScreen.js
+++ src/routes/ThematicScreen.js
@@ -1,9 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import { useParams } from 'react-router-dom'
+import LinkContainer from 'react-router-bootstrap/LinkContainer'
 
-export default class ThematicScreen extends Component {
-  render() {
-    return (
-      <h1>Some text.</h1>
-    );
-  }
+export default function ThematicScreen () {
+  return (
+    <>
+      <ABreadcrumb />
+    </>
+  );
+}
+
+function ABreadcrumb () {
+  const {thematicID} = useParams();
+  return (
+    <>
+      <Breadcrumb>
+        <LinkContainer to = "/">
+          <Breadcrumb.Item> Αρχική Σελίδα </Breadcrumb.Item>
+        </LinkContainer>
+        <Breadcrumb.Item active> Θεματική Ενότητα {thematicID} </Breadcrumb.Item>
+      </Breadcrumb>
+    </>
+  );
 }
