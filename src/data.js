670309cd8aeab2f926be8a7f3f5e8eb303b64530
GKouv89 <gina101@windowslive.com>
2022-05-08 12:22:17 -0700

    Color filter component implemented.
    A 'Remove Filter' functionality remains to be implemented.

--- src/data.js
+++ src/data.js
@@ -1,24 +1,41 @@
 let thematics = [
-    { 
-      _id: 1,
-      name: 'Περιβάλλον',
-      desc: 'Η πρώτη περιγραφή',
-      img_path: process.env.PUBLIC_URL + 'pubassets/environment.jpg'
-    },
-    { 
-      _id: 2,
-      name: 'Εργασία',
-      desc: 'Η 2η περιγραφή',
-      img_path: process.env.PUBLIC_URL + 'pubassets/jobs.jpg'
-    },
-    { 
-      _id: 3,
-      name: 'Μεταναστευτικό',
-      desc: 'Η 3η περιγραφή',
-      img_path: process.env.PUBLIC_URL + 'pubassets/immigration.jpg'
-    }
-  ];
-  
-  export function getThematics() {
-      return thematics;
+  { 
+    _id: 1,
+    name: 'Περιβάλλον',
+    desc: 'Η πρώτη περιγραφή',
+    img_path: process.env.PUBLIC_URL + 'pubassets/environment.jpg'
+  },
+  { 
+    _id: 2,
+    name: 'Εργασία',
+    desc: 'Η 2η περιγραφή',
+    img_path: process.env.PUBLIC_URL + 'pubassets/jobs.jpg'
+  },
+  { 
+    _id: 3,
+    name: 'Μεταναστευτικό',
+    desc: 'Η 3η περιγραφή',
+    img_path: process.env.PUBLIC_URL + 'pubassets/immigration.jpg'
   }
+];
+
+let colors = [
+  // {'name': '#9400D3'}, // Violet
+	// {'name': '#4B0082'}, // Indigo
+  
+  '#9400D3',
+  '#4B0082',
+  '#0000FF', // Blue	
+  '#00FF00', // Green
+	'#FFFF00', // Yellow
+	'#FF7F00', // Orange
+  '#FF0000', // Red
+];
+  
+export function getThematics() {
+    return thematics;
+}
+
+export function getColors() {
+  return colors;
+}
--- src/routes/ThematicScreen.js
+++ src/routes/ThematicScreen.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container'
+import React, { useState } from 'react';
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
 import { useParams } from 'react-router-dom'
 import LinkContainer from 'react-router-bootstrap/LinkContainer'
+import { getColors } from '../data'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+
 
 export default function ThematicScreen () {
   return (
     <>
-      <ABreadcrumb />
+      <Container fluid>
+        <Row>
+          <Col>
+            <ABreadcrumb />
+          </Col>
+          <Col>
+            <ColorFilter />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
@@ -22,6 +37,30 @@ function ABreadcrumb () {
         </LinkContainer>
         <Breadcrumb.Item active> Θεματική Ενότητα {thematicID} </Breadcrumb.Item>
       </Breadcrumb>
+    </>
+  );
+}
+
+function ColorFilter () {
+  const [radioValue, setRadioValue] = useState('None');
+
+  let filters = getColors();
+
+  return (
+    <>
+      <ButtonGroup>
+        {filters.map((color, idx) => 
+          <ToggleButton key={idx} 
+            id={`radio-${idx}`}
+            type="radio"
+            name="radio"
+            value={color}
+            checked={radioValue === color}
+            style={{backgroundColor: (radioValue === color) || (radioValue === 'None') ? color : "#FFFFFF", borderColor: color}}
+            onChange={(e) => setRadioValue(e.currentTarget.value)}
+          />                
+        )}
+      </ButtonGroup>
     </>
   );
 }
