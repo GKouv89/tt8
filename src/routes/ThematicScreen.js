babc690c6a82391875aee85b1fcebaff11c6f034
GKouv89 <gina101@windowslive.com>
2022-05-11 04:39:25 -0700

    Hardcoded shuffle works.
    Saving before expanding.

--- src/routes/ThematicScreen.js
+++ src/routes/ThematicScreen.js
@@ -2,21 +2,26 @@ import React, { Component, useState } from 'react';
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
 import { useParams } from 'react-router-dom'
 import LinkContainer from 'react-router-bootstrap/LinkContainer'
-import { getContentColors } from '../data'
+import { getContentColors, getContent } from '../data'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Card from 'react-bootstrap/Card';
 import ToggleButton from 'react-bootstrap/ToggleButton';
-import { CardGroup } from 'react-bootstrap';
 
 export default class ThematicScreen extends Component{
   constructor(props){
     super(props);
+    let contentSize = getContent().length;
+    let padding = this.props.gridSize-contentSize;
+    console.log(contentSize);
+    console.log(padding);
+    let elemOrder = this.makeRandomGridOrder(4).slice(0, 5);
     this.state = { 
-      gridHeight: 1, // Number of rows to render
-      gridWidth: 7, // Number of columns to render
+      gridSize: this.props.gridSize,
+      padding: padding,
+      elemOrder: elemOrder,
       enabledColor: 'All'
     };
   }
@@ -32,9 +37,26 @@ export default class ThematicScreen extends Component{
     this.setState(newState);
   }
   
+  makeRandomGridOrder(gridSize){
+    let allMyIndices = [...Array(gridSize).keys()].map(i => i);
+    var r, rand;
+    r = [];
+    while (allMyIndices.length){
+        rand = Math.floor(Math.random()*allMyIndices.length);
+        r.push(allMyIndices.splice(rand,1).pop());
+    }
+    return r;
+  }
+
+  makeFilterColors(col){
+    let colors = col.filter((v, i, a) => a.indexOf(v) === i);
+    colors.splice(colors.indexOf("None"), 1);
+    return colors;
+  }
+  
   render() {
-    const colors = getContentColors();
-    console.log('Selected axis is: ' + this.state.enabledColor);
+    const colors = getContentColors().slice(0, 3);
+    colors.push("None");
     return (
       <>
         <Container fluid>
@@ -43,32 +65,34 @@ export default class ThematicScreen extends Component{
               <ABreadcrumb />
             </Col>
             <Col xxl="auto">
-              <ColorFilter filters={colors.filter((v, i, a) => a.indexOf(v) === i)} callback={this.handleFilter.bind(this)}/>
+              <ColorFilter filters={this.makeFilterColors(colors)} callback={this.handleFilter.bind(this)}/>
             </Col>
           </Row>
         </Container>
-        <CuratedContentGrid colors={colors} enabledColor={this.state.enabledColor} handleClick={this.handleClick}/>
+        <CuratedContentGrid 
+          colCount={this.props.colCount} 
+          dudCount={this.state.padding}
+          colors={colors} 
+          order={this.state.elemOrder} 
+          enabledColor={this.state.enabledColor} 
+          handleClick={this.handleClick}/>
       </>
     );
   }
 }
 
 function CuratedContentGrid(props) {
-  
-  function createDuds(){
-    const duds = [];
-    for(let x = 0; x < 20 - (props.colors.length % 20); x++){
-      duds.push(<DudElement key={x}/>);
-    }
-    return duds;
-  }
-
   return (
     <>
       <Container>
-        <Row className="g-0 justify-content-center" xxl={20}>
-          {props.colors.map((color, idx) => <GridElement key={idx} enabledColor={props.enabledColor} color={color} handleClick={props.handleClick} /*order={Math.floor(Math.random() * 20) + 1}*/ /> )}
-          {createDuds()}
+        <Row className="g-0 justify-content-center" xxl={props.colCount}>
+          {props.order.map((order, idx) => 
+            <Col key={idx}>
+              <GridElement
+              enabledColor={props.enabledColor}
+              color={props.colors[order]}
+              handleClick={props.handleClick} />
+            </Col>)}
         </Row>
       </Container>
     </>
@@ -78,29 +102,87 @@ function CuratedContentGrid(props) {
 function GridElement(props) {
   return(
     <>
-      <Col xxl={{ order: props.order }}>
-        <Card>
-          <Card.Body 
-            style={{ backgroundColor: props.color, borderColor: props.color, visibility: (props.enabledColor === props.color) || (props.enabledColor === 'All') ? "visible" : "hidden" }} 
-            as="button" 
-            disabled={ (props.enabledColor === props.color) || (props.enabledColor === 'All') ? false : true }
-            onClick={props.handleClick}>
-          </Card.Body>
-        </Card>
-      </Col>
+      <Card>
+        <Card.Body 
+          style = {{
+            backgroundColor: (props.color !== "None") ? props.color : undefined,
+            borderColor: (props.color !== "None") ? props.color : undefined,
+            visibility: ((props.enabledColor === props.color) || (props.enabledColor === 'All')) && (props.color !== "None") ? "visible" : "hidden"
+          }}
+          // style={{ 
+          //   backgroundColor: props.color, 
+          //   borderColor: props.color, 
+          //   visibility: (props.enabledColor === props.color) || (props.enabledColor === 'All') ? "visible" : "hidden" }} 
+          as="button" 
+          disabled={ (props.enabledColor === props.color) || (props.enabledColor === 'All') ? false : true }
+          onClick={props.handleClick}>
+        </Card.Body>
+      </Card>
     </>
   );
 }
 
-function DudElement(props){
-  return(
+function HardcodedGrid(props) {
+  return (
     <>
-      <Col>
-        <Card>
-          <Card.Body as="button" disabled={true}>
-          </Card.Body>
-        </Card>
-      </Col>
+      <Container>
+        <Row className="g-0 justify-content-center" xxl='7'>
+          <Col key={1} className="order-xxl-3">
+            <Card>
+              <Card.Body 
+                style={{ backgroundColor: props.colors[0], borderColor: props.colors[0], visibility: (props.enabledColor === props.colors[0]) || (props.enabledColor === 'All') ? "visible" : "hidden" }} 
+                as="button" 
+                disabled={ (props.enabledColor === props.colors[0]) || (props.enabledColor === 'All') ? false : true }
+                onClick={props.handleClick}>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col key={2} className="order-xxl-0">
+            <Card>
+              <Card.Body 
+                style={{ backgroundColor: props.colors[1], borderColor: props.colors[1], visibility: (props.enabledColor === props.colors[1]) || (props.enabledColor === 'All') ? "visible" : "hidden" }} 
+                as="button" 
+                disabled={ (props.enabledColor === props.colors[1]) || (props.enabledColor === 'All') ? false : true }
+                onClick={props.handleClick}>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col key={3} className="order-xxl-4">
+            <Card>
+              <Card.Body 
+                style={{ backgroundColor: props.colors[2], borderColor: props.colors[2], visibility: (props.enabledColor === props.colors[2]) || (props.enabledColor === 'All') ? "visible" : "hidden" }} 
+                as="button" 
+                disabled={ (props.enabledColor === props.colors[2]) || (props.enabledColor === 'All') ? false : true }
+                onClick={props.handleClick}>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col key={4} className="order-xxl-2">
+            <Card>
+              <Card.Body style={{ visibility: "hidden" }} as="button" disabled={true}>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col key={5} className="order-xxl-5">
+            <Card>
+              <Card.Body style={{ visibility: "hidden" }} as="button" disabled={true}>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col key={6} className="order-xxl-1">
+            <Card>
+              <Card.Body style={{ visibility: "hidden" }} as="button" disabled={true}>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col key={7} className="order-xxl-6">
+            <Card>
+              <Card.Body style={{ visibility: "hidden" }} as="button" disabled={true}>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
@@ -158,31 +240,4 @@ function ColorFilter (props) {
       </ToggleButton>
     </>
   );
-}
-
-function AGrid () {
-  const colors = getContentColors();
-  
-  function handleClick() {
-    console.log('I clicked');
-  }
-
-  return (
-    <>
-      <Container>
-        <Row>
-          <Col>
-            <CardGroup> 
-              {colors.map((color, idx) =>               
-                <Card key={idx}>
-                  <Card.Body style={{ backgroundColor: color }} as="button" onClick={handleClick}>
-                  </Card.Body>
-                </Card>
-              )}
-            </CardGroup>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  )
 }
