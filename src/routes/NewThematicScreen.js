8240077cd42547d601f1389511153a0ea781ffef
GKouv89 <gina101@windowslive.com>
2022-05-27 08:11:38 -0700

    A first version of the grid is ready.
    Filter displays properly, episode tiles in place,  random order not yet reinstated.

--- src/routes/NewThematicScreen.js
+++ src/routes/NewThematicScreen.js
@@ -9,8 +9,10 @@ import Button from 'react-bootstrap/Button'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 
-import { getThematics, getAxisNames } from '../data.js'
+import { getThematics, getAxisNames, getContentOfThematic, getThematicEpisodes } from '../data.js'
 import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from "react-router-dom"
+
 
 export default function ThematicScreenWrapper(props){
     let {thematicID} = useParams();
@@ -54,9 +56,7 @@ function ThematicGridHeader(props){
             }
           </Button>
         </Col>
-        <Col xxl={4}>
-          <ColorFilter id={props.id}/>
-        </Col>
+        <ColorFilterColumnWrapper id={props.id} />
         <Col xxl={4}>
             <LinkContainer to="/">
               <Button variant='light' className="rounded-0">
@@ -69,52 +69,173 @@ function ThematicGridHeader(props){
   );
 }
 
+function ThematicGridBody(props){
+  // [] = useState();
+  const gridSize = 30;
+  let content = getContentOfThematic(props.id);
+  // let colors = getContentColors(content);
+  // // Account for episode tiles here, before padding calculation.
+  let episodes = getThematicEpisodes(props.id);
+  let numberOfSquares = content.length + episodes.length;
+  let numberOfPadding = gridSize - numberOfSquares;
+  
+  const makeRandomGridOrder = (gridSize) => {
+    let allMyIndices = [...Array(gridSize).keys()].map(i => i);
+    var r, rand;
+    r = [];
+    while (allMyIndices.length){
+        rand = Math.floor(Math.random()*allMyIndices.length);
+        r.push(allMyIndices.splice(rand,1).pop());
+    }
+    return r;
+  }
+
+  const makePaddedGridContent = (content, episodes, numberOfPadding) => {
+    let contentArray = [];
+    content.map((content, idx) => (
+      contentArray.push(<GridSquare key={idx} content={content}/>)
+    ));
+    episodes.map((episodeno, idx) => (
+      contentArray.push(<EpisodeSquare key={idx} thematicid={props.id} epno={episodeno} />)
+    ));
+    for(let x = 0; x < numberOfPadding; x++){
+      contentArray.push(<EmptySquare key={x}/>);
+    }
+    return contentArray;
+  }
+
+  let gridOrder = makeRandomGridOrder(gridSize);
+  let gridContent = makePaddedGridContent(content, episodes, numberOfPadding);
+  // let padding = this.props.gridSize-contentSize - episodes.length;
+  // let elemOrder = this.makeRandomGridOrder(this.props.gridSize);
+  // console.log(elemOrder);
+  // console.log(this.props.id);
+  // this.state = { 
+  //   content: content,
+  //   colors: colors,
+  //   episodes: episodes,
+  //   gridSize: this.props.gridSize,
+  //   padding: padding,
+  //   elemOrder: elemOrder,
+  //   enabledColor: 'All'
+  // };
+  // console.log(content);
+  // console.log(episodes);
+  // console.log(numberOfSquares);
+  return(
+    <>
+      <Container>
+        <Row className="border border-light">
+          {gridContent}
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+function GridSquare(props){
+  return(
+    <>
+      <Col xxl={2} className="border border-light gridsquare m-0 p-0">
+        <Card className={"border-light gridsquare axis" + props.content._axis_id}>
+          <Card.Body 
+            className={"axis" + props.content._axis_id}
+            as="button">
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
+  );
+}
+
+function EpisodeSquare(props){
+  return(
+    <>
+      <Col xxl={2} className="border border-light gridsquare m-0 p-0">
+        <Card className="gridsquare episode-tile-new border-light">
+          <Card.Body 
+            className="episode-tile-new"
+            as="button">
+            <Link to={`/${props.thematicid}/episodes/${props.epno}`} style={{color: "white", textDecoration: "none"}}>
+              Επεισόδιο {props.epno}
+            </Link>
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
+  );
+}
+
+function EmptySquare(){
+  return(
+    <>
+      <Col xxl={2} className="border border-light gridsquare m-0 p-0">
+        <Card className="gridsquare border-light empty">
+          <Card.Body 
+            className="empty"
+            as="button"
+            disabled="true">
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
+  );
+}
+
+
+
 function ColorFilter(props){
   const [expanded, setExpanded] = useState(false);
   const [hovered, setHovered] = useState('None');
   let axisnames = getAxisNames();
+
   return (
     <>
-      {/* <ButtonGroup>
-        {props.filters.map((color, idx) => 
-          <ToggleButton key={idx} 
-            id={idx}
-            type="radio"
-            name="radio"
-            value={color}
-            checked={radioValue === color}
-            style={{backgroundColor: (radioValue === color) || (radioValue === 'All') ? color : "#FFFFFF", borderColor: color}}
-            onChange={(e) => {
-              props.callback(parseInt(e.currentTarget.id) + 1);
-              setRadioValue(e.currentTarget.value);
-            }}
-          />                
-        )}
-      </ButtonGroup>
-      <ToggleButton 
-          key={props.filters.length} 
-          id={`${props.filters.length}`}
-          type="radio"
-          name="radio"
-          value='All'
-          checked={radioValue === 'All'}
-          variant="light"
-          onChange={(e) => 
-            {
-              props.callback(e.currentTarget.value);
-              setRadioValue(e.currentTarget.value);
-            }}
-        >
-          Καθαρισμός Φίλτρου
-      </ToggleButton> */}
-      <DropdownButton id="dropdown-item-button" variant={(expanded) ? "thematic" + props.id : "descselected"} onClick={() => setExpanded(!expanded)} title="Φίλτρα">
+      <DropdownButton id="dropdown-item-button" variant={(expanded) ? "descselected" : "thematic" + props.id } onClick={() => setExpanded(!expanded)} title="Φίλτρα">
         {axisnames.map((name, idx) => 
-          <Dropdown.Item key={idx} as="button" className={"axis" + (++idx)} onMouseOver={() => setHovered(idx)} onMouseOut={() => setHovered('None')}>
+          <Dropdown.Item key={idx} as="button" className={"filter" + (++idx)} onMouseOver={() => setHovered(idx)} onMouseOut={() => setHovered('None')} onClick={() => props.callback((idx))}>
             <p style={{visibility: (hovered == idx) ? "visible": "hidden"}}>
               {name}
             </p>
           </Dropdown.Item>)}
-      </DropdownButton>
+      </DropdownButton>    
+    </>
+  );
+}
+
+function ClearFilter(props){
+  return (
+    <>
+      <Button variant="descselected" onClick={() => props.callback()}> 
+        Πίσω
+        <i className="bi bi-arrow-left"></i>
+      </Button>
+    </>
+  );
+}
+
+function ColorFilterColumnWrapper(props){
+  const [filterChosen, setFilterChosen] = useState(false);
+  const [colorChosen, setColorChosen] = useState('None');
+
+  const clearFilterCallback = () => {
+    setFilterChosen(!filterChosen);
+    setColorChosen('None');
+  }
+
+  const filterCallback = (id) => {
+    setFilterChosen(!filterChosen);
+    setColorChosen(id);
+  }
+  console.log('filterchosen: ' + filterChosen);
+  console.log('colorchosen: ' + colorChosen);
+  return (
+    <>
+      <Col xxl={4} className={(filterChosen) ? "border border-light descselected" : "border border-light"}>
+      {
+        (filterChosen) ?  <ClearFilter callback={clearFilterCallback}/> : <ColorFilter id={props.id} callback={filterCallback} />
+      }
+      </Col>
     </>
   );
 }
@@ -139,6 +260,9 @@ function ThematicGrid(props) {
         </Row>
         <Row>
           <ThematicGridHeader name={content.name} desc={content.desc} id={props.id} callback={descButtonCallback} open={open}/>
+        </Row>
+        <Row>
+          <ThematicGridBody id={props.id}/>
         </Row>
       </Container>
     </Container>
